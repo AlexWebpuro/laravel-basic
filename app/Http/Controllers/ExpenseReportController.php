664422607eeef6a23fb731c572b9004b49a6b0b0
Alex Monroy <alexwebpuro@gmail.com>
2023-09-20 13:24:57 -0500

    Add triggered to send mail

--- app/Http/Controllers/ExpenseReportController.php
+++ app/Http/Controllers/ExpenseReportController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SummaryReport;
+use Illuminate\Support\Facades\Mail;
 use App\Models\ExpenseReport;
 use Illuminate\Http\Request;
 
@@ -116,8 +118,10 @@ class ExpenseReportController extends Controller
     /**
      * Send mail report.
      */
-    public function sendMail($id) {
+    public function sendMail( Request $request, $id) {
         $report =  ExpenseReport::findOrFail($id);
-        return $report;
+        Mail::to( $request->get('email'))->send( new SummaryReport( $report ));
+        
+        return redirect('expense_reports/' . $id );
     }
 }
